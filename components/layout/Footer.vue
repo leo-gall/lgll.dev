4f7b3276fe70354ed7728fb4adc8e9461e1fbfed
Leo Gall <gall.dev@proton.me>
2024-12-29 14:10:14 +0100

    add twitch profile

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -54,7 +54,7 @@
         to="https://bsky.app/profile/leogall.bsky.social"
         target="_blank"
         v-tooltip="`leogall`"
-        data-umami-event="social-redirect-github"
+        data-umami-event="social-redirect-bluesky"
         aria-label="go to Leo Gall's BlueSky profile"
       >
         <svg fill="none" viewBox="0 0 64 57" width="30px" height="30px">
@@ -123,6 +123,35 @@
                 fill-rule="nonzero"
               ></path>
             </g>
+          </g>
+        </svg>
+      </NuxtLink>
+      <NuxtLink
+        to="https://www.twitch.tv/lglldev"
+        target="_blank"
+        v-tooltip="`lglldev`"
+        data-umami-event="social-redirect-twitch"
+        aria-label="go to Leo Gall's Twitch channel"
+      >
+        <svg
+          width="30px"
+          height="30px"
+          fill="#fff"
+          viewBox="0 0 32 32"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#fff"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M26.711 14.929l-4.284 4.284h-4.285l-3.749 3.749v-3.749h-4.82v-16.067h17.138zM8.502 1.004l-5.356 5.356v19.279h6.427v5.356l5.356-5.356h4.284l9.641-9.64v-14.996zM21.356 6.895h2.142v6.427h-2.142zM15.464 6.895h2.143v6.427h-2.144z"
+            ></path>
           </g>
         </svg>
       </NuxtLink>
